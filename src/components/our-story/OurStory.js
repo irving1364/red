9504d950036abe6831613fd320a3a793edd7dcf9
irving1364 <irving1364@gmail.com>
2024-04-04 17:10:04 -0400

    Add files via upload

--- src/components/our-story/OurStory.js
+++ src/components/our-story/OurStory.js
@@ -14,75 +14,29 @@ const OurStory = () => {
         <div className="row justify-content-between">
           <div className="col-lg-5 col-md-12 order-lg-1">
             <div className="section-heading sticky-sidebar">
-              <h4 className="h5 text-primary">Our Story</h4>
-              <h2>A Great Story Starts with a Friendly Team</h2>
+              <h4 className="h5 text-primary">Conocenos</h4>
+              <h2>Red de abusos eclesiasticos de Venezuela</h2>
               <p>
-                Globally e-enable principle-centered e-business before dynamic
-                quality vectors cross-media materials before proactive
-                outsourcing leverage other is vertical technology leadership.{' '}
+              Somos una red, sin fines de lucro, de venezolanos que hemos sufrido abuso sexual, 
+              unidos para concientizar a la sociedad de la realidad de las victimas de abuso, 
+              brindar un acompañamiento a las víctimas y evidenciar las decadentes respuestas 
+              de la iglesia que vulneran la infancia y la dignidad de los individuos.{' '}
               </p>
-              <div className="mt-4">
-                <h6 className="mb-3">We Are Awarded By-</h6>
-                <img
-                  src="/awards-01.svg"
-                  alt="awards"
-                  className="me-4 img-fluid"
-                />
-                <img src="/awards-02.svg" alt="awards" className="img-fluid" />
-              </div>
+              
             </div>
           </div>
           <div className="col-lg-6 col-md-12 order-lg-0">
             <div className="story-grid-wrapper position-relative">
               <ul className="position-absolute animate-element parallax-element shape-service d-none d-lg-block">
-                <li className="layer" data-depth="0.02">
-                  <Image
-                    width={129}
-                    height={72}
-                    src="/color-shape/image-2.svg"
-                    alt="shape"
-                    className="img-fluid position-absolute color-shape-2 z-5"
-                  />
-                </li>
-                <li className="layer" data-depth="0.03">
-                  <Image
-                    width={129}
-                    height={72}
-                    src="/color-shape/feature-3.svg"
-                    alt="shape"
-                    className="img-fluid position-absolute color-shape-3"
-                  />
-                </li>
+               
               </ul>
 
-              <div className="story-grid rounded-custom bg-dark overflow-hidden position-relative">
+              <div className=" rounded-custom bg-dark overflow-hidden position-relative">
                 <div className="story-item bg-light border">
-                  <h3 className="display-5 fw-bold mb-1 text-success">550K+</h3>
-                  <h6 className="mb-0">Active Users</h6>
-                </div>
-                <div className="story-item bg-white border">
-                  <h3 className="display-5 fw-bold mb-1 text-primary">250+</h3>
-                  <h6 className="mb-0">Team Members</h6>
-                </div>
-                <div className="story-item bg-white border">
-                  <h3 className="display-5 fw-bold mb-1 text-dark">$20M+</h3>
-                  <h6 className="mb-0">Revenue Per/Year</h6>
-                </div>
-                <div className="story-item bg-light border">
-                  <h3 className="display-5 fw-bold mb-1 text-warning">
-                    8 Years
-                  </h3>
-                  <h6 className="mb-0">In Business</h6>
-                </div>
-                <div className="story-item bg-light border">
-                  <h3 className="display-5 fw-bold mb-1 text-danger">425+</h3>
-                  <h6 className="mb-0">Clients Worldwide</h6>
-                </div>
-                <div className="story-item bg-white border">
-                  <h3 className="display-5 fw-bold mb-1 text-primary">855+</h3>
-                  <h6 className="mb-0">Projects Completed</h6>
+                  <img width="70%" src="/logoColor.png" alt="about"  className="rounded mx-auto d-block  shadow"/>
                 </div>
               </div>
+
             </div>
           </div>
         </div>
